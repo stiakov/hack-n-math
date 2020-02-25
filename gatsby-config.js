e6383a660fffd6314ffd36ee71f869fcc4c8222b
Santiago Torres G <thdealer@gmail.com>
2020-02-25 12:44:45 -0500

    Update plugin setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,18 @@
 module.exports = {
   siteMetadata: {
     title: 'Santiago Torres - Full-Stack Developer',
-    description: 'Santiago Torres Remote Full-Stack Web Developer | Rails | React | GraphQL | GatsbyJS | Ruby | JavaScript | JamStack | NodeJS | Software | Design | Software | Development | Colombia',
+    description: 'Santiago Torres Remote Full-Stack Web Developer Portfolio | Rails | React | GraphQL | GatsbyJS | Ruby | JavaScript | Python | Clojure | JamStack | NodeJS | Software | Design | Development | Engineering | Computer Science | Colombia',
   },
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/Layout.jsx'),
+        },
+      },
+    },
+  ],
 };
