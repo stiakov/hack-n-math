ba9d6a9b2802dc8e0df3ebbba002760f5b73335d
Santiago Torres G <thdealer@gmail.com>
2020-02-25 09:28:10 -0500

    Add stickler fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,16 +15,16 @@ const descriptionStyle = css`
   font-family: Cabin;
 `;
 
-export default () => {
-  return (
-    <Layout>
-      <h1 css={nameStyle}>Santiago Torres</h1>
-      <h3 css={descriptionStyle}>
-        Full-Stack Web Developer Ipsum officia quis labore ipsum exercitation
-        esse laborum ipsum velit mollit esse est dolore. Elit reprehenderit elit
-        veniam cillum sunt consequat adipisicing. Eu consequat excepteur laboris
-        non aliqua. Dolore deserunt nulla magna laboris Lorem.
-      </h3>
-    </Layout>
-  )
-}
+const Index = () => (
+  <Layout>
+    <h1 css={nameStyle}>Santiago Torres</h1>
+    <h3 css={descriptionStyle}>
+      Full-Stack Web Developer Ipsum officia quis labore ipsum exercitation
+      esse laborum ipsum velit mollit esse est dolore. Elit reprehenderit elit
+      veniam cillum sunt consequat adipisicing. Eu consequat excepteur laboris
+      non aliqua. Dolore deserunt nulla magna laboris Lorem.
+    </h3>
+  </Layout>
+);
+
+export default Index;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Layout from './components/Layout';
 
-export default () => {
-  return (
-    <Layout>
-      <h1>From Projects</h1>
-    </Layout>
-  )
-}
+const Projects = () => (
+  <Layout>
+    <h1>From Projects</h1>
+  </Layout>
+);
+
+export default Projects;
