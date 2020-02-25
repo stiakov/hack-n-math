4d11eb0ded4cc234b5f5954c082bd044a94382f8
Santiago Torres G <thdealer@gmail.com>
2020-02-24 23:43:23 -0500

    Update pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,30 @@
-import React from "react";
-import { Link } from "gatsby";
+import React from 'react';
+import { css } from '@emotion/core';
 import Layout from './components/Layout';
+
+const nameStyle = css`
+  text-decoration: none;
+  color: #8c8c8c;
+  font-size: 3rem;
+  font-weight: bold;
+`;
+
+const descriptionStyle = css`
+  text-decoration: none;
+  color: #404040;
+  font-family: Cabin;
+`;
 
 export default () => {
   return (
     <Layout>
-      <h1>Santiago Torres G – Full-Stack Web Developer</h1>
-      <Link to="/projects/">Go to projects &rarr;</Link>
+      <h1 css={nameStyle}>Santiago Torres</h1>
+      <h3 css={descriptionStyle}>
+        Full-Stack Web Developer Ipsum officia quis labore ipsum exercitation
+        esse laborum ipsum velit mollit esse est dolore. Elit reprehenderit elit
+        veniam cillum sunt consequat adipisicing. Eu consequat excepteur laboris
+        non aliqua. Dolore deserunt nulla magna laboris Lorem.
+      </h3>
     </Layout>
   )
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,12 +1,10 @@
-import React from "react";
-import { Link } from "gatsby";
-import Layout from "./components/Layout";
+import React from 'react';
+import Layout from './components/Layout';
 
 export default () => {
   return (
     <Layout>
       <h1>From Projects</h1>
-      <Link to="/">&larr; Go home</Link>
     </Layout>
   )
 }
