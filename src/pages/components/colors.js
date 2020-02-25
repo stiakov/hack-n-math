7ee250b4fa32a8794921c19258e10b393740a8a9
Santiago Torres G <thdealer@gmail.com>
2020-02-24 23:41:39 -0500

    Add color vars

---
+++ src/pages/components/colors.js
@@ -0,0 +1,7 @@
+export const light = 'rgba(242, 242, 242, 1)';
+export const lightMedium = 'rgba(191, 191, 191, 1)';
+export const neutro = 'rgba(140, 140, 140, 1)';
+export const darkMedium = 'rgba(63, 63, 63, 1)';
+export const dark = 'rgba(38, 38, 38, 1)';
+export const black = 'rgba(0, 0, 0, 1)';
+export const white = 'rgba(255, 255, 255, 1)';
