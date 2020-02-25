956b651ef26aaadeb8ac48384f3f5ade542ec5ec
Santiago Torres G <thdealer@gmail.com>
2020-02-25 10:50:56 -0500

    Add Helmet to add title and description to our website

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: ['gatsby-plugin-emotion'],
+  siteMetadata: {
+    title: 'Santiago Torres - Full-Stack Developer',
+    description: 'Santiago Torres Remote Full-Stack Web Developer | Rails | React | GraphQL | GatsbyJS | Ruby | JavaScript | JamStack | NodeJS | Software | Design | Software | Development | Colombia',
+  },
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
 };
